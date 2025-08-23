a2c45b087c828b6be48128533325578b9b0708a5
Obito-git <antonmyroshnychenko@gmail.com>
2025-08-23 22:52:24 +0200

    continue refactoring. print is going to be moved into class_file crate under feature flag, it makes much more sense, we don't need it at runtime. and I need to focus on file_class crate and start writing tests.

---
+++ src/indent_write.rs
@@ -0,0 +1,73 @@
+use std::fmt::{self};
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! pretty_try {
+    ($ind:expr, $expr:expr) => {
+        match $expr {
+            Ok(v) => v,
+            Err(e) => {
+                let _ = ::std::writeln!($ind, "\nprint error: {}", e);
+                return Err(::std::fmt::Error);
+            }
+        }
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! pretty_class_name_try {
+    ($ind:expr, $expr:expr) => {
+        match $expr {
+            Ok(class) => class.replace("/", "."),
+            Err(e) => {
+                let _ = ::std::writeln!($ind, "\nprint error: {}", e);
+                return Err(::std::fmt::Error);
+            }
+        }
+    };
+}
+
+pub struct Indented<'a> {
+    inner: &'a mut dyn fmt::Write,
+    unit: &'static str,
+    level: usize,
+    at_line_start: bool,
+}
+
+impl<'a> Indented<'a> {
+    pub fn new(inner: &'a mut dyn fmt::Write) -> Self {
+        Self {
+            inner,
+            unit: "  ",
+            level: 0,
+            at_line_start: true,
+        }
+    }
+
+    pub fn with_indent<F>(&mut self, f: F) -> fmt::Result
+    where
+        F: FnOnce(&mut Self) -> fmt::Result,
+    {
+        self.level += 1;
+        let res = f(self);
+        self.level -= 1;
+        res
+    }
+}
+
+impl fmt::Write for Indented<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for chunk in s.split_inclusive('\n') {
+            if self.at_line_start {
+                for _ in 0..self.level {
+                    self.inner.write_str(self.unit)?;
+                }
+                self.at_line_start = false;
+            }
+            self.inner.write_str(chunk)?;
+            self.at_line_start = chunk.ends_with('\n');
+        }
+        Ok(())
+    }
+}
