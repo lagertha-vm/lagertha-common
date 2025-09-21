a214114dcba5146bdb6ea4600a98d03aa6f2517f
Obito-git <antonmyroshnychenko@gmail.com>
2025-09-21 21:46:40 +0200

    add new tests, and start creating tests for the vm

--- src/jtype.rs
+++ src/jtype.rs
@@ -45,7 +45,9 @@ pub enum Type {
 pub type HeapAddr = usize;
 
 //TODO: draft. refactor
-#[derive(Debug, Clone, PartialEq)]
+//TODO: serializes right now only for runtime crate tests, but can't move it to dev deps
+//TODO: the whole common crate should be rethought
+#[derive(Debug, Clone, PartialEq, serde::Serialize)]
 pub enum Value {
     Integer(i32),
     Long(i64),
