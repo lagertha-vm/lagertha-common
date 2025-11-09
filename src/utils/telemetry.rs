d90b1fa9dbd0ec6e4f11c4b91145b6e29fa186b7
Obito-git <antonmyroshnychenko@gmail.com>
2025-11-09 23:07:37 +0100

    support more instructions, moving forward. fixed instanceof which was completely broken

--- src/utils/telemetry.rs
+++ src/utils/telemetry.rs
@@ -5,9 +5,10 @@ pub fn init_tracing() {
         .with_env_filter(
             EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("trace")),
         )
-        .with_target(true)
-        .with_file(true)
-        .with_line_number(true)
+        .without_time()
+        //.with_target(true)
+        //.with_file(true)
+        //.with_line_number(true)
         .compact()
         .try_init();
 }
